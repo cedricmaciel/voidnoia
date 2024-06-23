b2c9d76277092ae53796359a40e2cd8669659c47
cedricmaciel <164974899+cedricmaciel@users.noreply.github.com>
2024-06-23 18:08:23 -0300

    Update DataLoader.java

--- DataLoader.java
+++ DataLoader.java
@@ -5,13 +5,12 @@ import org.nd4j.linalg.dataset.api.preprocessor.NormalizerMinMaxScaler;
 public class DataLoader {
 
     public static DataSetIterator loadTextData(String filePath) {
-        // Implementação para carregar e preparar dados de texto
-        // Exemplo: leitura de arquivos CSV, tokenização de texto, etc.
+       
     }
 
     public static DataSetIterator loadImageData(String directoryPath) {
-        // Implementação para carregar e preparar dados de imagem
-        // Exemplo: carregar imagens de um diretório e convertê-las para DataSet
+        // carregar e preparar dados de imagem
+        // carregar imagens de um diretório  para DataSet
     }
 
     public static void normalizeData(DataSetIterator data) {
